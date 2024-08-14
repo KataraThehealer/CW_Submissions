76ee17a512b7e5f05fac0bc932bc4aa5aeeccc43
KataraThehealer <hawkmoth10@gmail.com>
2024-08-14 10:30:37 -1000

    Create Hello_World.java

---
+++ Hello_World.java
@@ -0,0 +1,19 @@
+/*
+ * This us the very first assignment. It does two simple prints and then I submit it.
+ */
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Task 1: print out "Hello World"
+		System.out.print("Hello World");
+		
+		
+		//Task 2: Print out Hello World again 
+		//and then a third time on next line
+		System.out.println("Hello World");
+		System.out.print("Hello World");
+
+	}
+}
+	
