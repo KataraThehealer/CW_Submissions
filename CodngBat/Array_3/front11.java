304beb655e6ac8813001fa4cd6983839cc6a0aba
KataraThehealer <hawkmoth10@gmail.com>
2025-02-15 11:58:27 -1000

    Create front11.java

---
+++ CodngBat/Array_3/front11.java
@@ -0,0 +1,24 @@
+public int[] front11(int[] a, int[] b) {
+  if(a.length == 0 && b.length == 0)
+  {
+    return a;
+  }
+  
+  if(a.length ==0)
+  {
+    int[] myArray = new int[] {b[0]};
+    return myArray;
+  }
+  
+  if(b.length == 0)
+  {
+    int[] myArray = new int[] {a[0]};
+    return myArray;
+  }
+  
+  else
+  {
+    int[] myArray = new int[] {a[0], b[0]};
+    return myArray;
+  }
+}
