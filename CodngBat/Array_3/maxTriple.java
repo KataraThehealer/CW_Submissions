8d496d71448d81025ae97ae949284d0788f599d6
KataraThehealer <hawkmoth10@gmail.com>
2025-02-15 11:41:43 -1000

    Update maxTriple.java

--- CodngBat/Array_3/maxTriple.java
+++ CodngBat/Array_3/maxTriple.java
@@ -1,6 +1,24 @@
 public int maxTriple(int[] nums) {
-  int largest = Math.max(nums[0], nums[nums.length/2]);
+  public int maxTriple(int[] nums) {
+ int largest = 0; 
+  int first = nums[0];
+  int middle = nums[nums.length/2];
+  int last = nums[nums.length-1];
   
-  largest = Math.max(largest, nums[nums.length-1]);
-  return largest; 
+  if(first > middle && first > last)
+  {
+    return first;
+  }
+  
+  else if(middle > first && middle > last)
+  {
+    return middle;
+  }
+  
+  else
+  {
+    return last;
+  }
+}
+
 }
